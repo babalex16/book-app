f52442d5831856f7ce62095aae727380ff19a3da
babalex16 <82571265+babalex16@users.noreply.github.com>
2023-05-20 16:08:51 +0200

    Routing and folders , navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BooksPageComponent } from './pages/books-page/books-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BooksPageComponent},
+  { path: 'about', component: AboutPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
+import { BooksPageComponent } from './pages/books-page/books-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    SearchFormComponent,
+    BooksPageComponent,
+    AboutPageComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
