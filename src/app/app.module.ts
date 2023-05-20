1ebeae7073790f182ff3da5dcc5eae1b3d5a95b1
babalex16 <82571265+babalex16@users.noreply.github.com>
2023-05-20 14:41:36 +0200

    navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './components/navigation/navigation.component';
+
 import { BookListComponent } from './components/book-list/book-list.component';
 import { SearchFormComponent } from './components/search-form/search-form.component';
+import { BooksPageComponent } from './pages/books-page/books-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
     BookListComponent,
-    SearchFormComponent
+    SearchFormComponent,
+    BooksPageComponent,
+    AboutPageComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
