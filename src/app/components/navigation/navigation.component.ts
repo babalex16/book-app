96ce9c28baac072105baa6ea60a2bcc1f69dcc86
babalex16 <82571265+babalex16@users.noreply.github.com>
2023-05-21 22:35:10 +0200

    navbar changed

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { Collapse, Dropdown, initTE } from 'tw-elements';
+
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
 })
 export class NavigationComponent {
-
+  ngOnInit() {
+      initTE({ Collapse, Dropdown });
+  }
 }
--- src/app/components/search-form/search-form.component.ts
+++ src/app/components/search-form/search-form.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({ 
     selector: 'app-search-form', 
-    templateUrl: './search-form.component.html'
+    templateUrl: './search-form.component.html',
+    styleUrls: ['./search-form.component.css']
  }) 
  export class SearchFormComponent {
     constructor() { } 
